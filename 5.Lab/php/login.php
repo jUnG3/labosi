d97ccd58f3ca496e5579d3a77e3167efc3f8bcfc
jUnG3 <root@precise32.(none)>
2014-06-17 21:24:02 +0000

    jos jedan

---
+++ 5.Lab/php/login.php
@@ -0,0 +1,36 @@
+﻿<?php
+	include 'baza.php';
+	
+	
+		if (!isset($_SESSION)) { session_start(); }
+		
+		$baza		= spojiSe();
+		
+		
+		$userName	= $_POST["username"];
+		$password	= md5($_POST["password"]);
+		
+		$stmt		= $baza->prepare("SELECT COUNT(*) AS broj FROM korisnici WHERE username = ? AND password = ?;");
+		$stmt->bind_param('ss', $userName, $password);
+		$stmt->execute();
+		$stmt->bind_result($broj);
+		
+	
+		$result		= $stmt->fetch();
+		echo $broj;
+		
+		if($broj == 1){
+		
+			$_SESSION['username'] 	= $_POST["username"];
+			header('Location: ../html/podaci.html');
+			
+		}
+		else{
+			
+			session_unset();
+			session_destroy();
+		}
+		
+		$stmt->close();
+		mysqli_close($baza);
+?>
