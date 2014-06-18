4dfbaf7b34bf432a05b98f0e9e9642127678a266
jUnG3 <root@precise32.(none)>
2014-06-18 09:34:25 +0000

    Upload 8. vjezbe, 6. nisam mogao napraviti neradi mi URL rewrite u virtualnoj masini

---
+++ 8.Lab/js/podaci.js
@@ -0,0 +1,24 @@
+﻿function username(){
+
+	$(document).ready(function(){
+	
+		$.ajax({
+			type: "GET",
+			url: "../php/controller.php?f=getUsername",
+			success: function(username){
+			
+				$("#korisnik").html(username);
+			}
+		});
+	});
+}
+
+$(document).ready(function(){
+
+	$.ajax({
+			type: "GET",
+			url: "../php/controller.php?f=jelLogiran",
+			success: function(username){
+			}
+		});
+});
